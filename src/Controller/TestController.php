1eee69081b4ab6b1ddde657fe46337d2d23072d7
walid-benaissa <walid.benaissa@esprit.tn>
2023-03-28 21:51:37 +0100

    dependencies

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -16,7 +15,7 @@ class TestController extends AbstractController
             'controller_name' => 'ClassroomController',
         ]);
     }
-    
+
     #[Route('/front', name: 'app_test1')]
     public function front(): Response
     {
