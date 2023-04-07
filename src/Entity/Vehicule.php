067b9f2a10d7987b8aef8337574109b74c110c22
walid-benaissa <walid.benaissa@esprit.tn>
2023-04-07 09:09:33 +0100

    ok

--- src/Entity/Vehicule.php
+++ src/Entity/Vehicule.php
@@ -142,7 +142,7 @@ class Vehicule
         return $this;
     }
 
-    public function getPromotion(): ?Promotion
+    /* public function getPromotion(): ?Promotion
     {
         return $this->Promotion;
     }
@@ -152,7 +152,7 @@ class Vehicule
         $this->Promotion = $Promotion;
 
         return $this;
-    }
+    } */
 
     public function getDisponibilite(): ?string
     {
