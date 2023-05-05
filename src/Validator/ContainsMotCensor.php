aa821a39f3980189423bb6c6902cd82e6775fe8d
walid-benaissa <walid.benaissa@esprit.tn>
2023-05-05 07:58:05 +0100

    fixes

--- src/Validator/ContainsMotCensor.php
+++ src/Validator/ContainsMotCensor.php
@@ -7,7 +7,12 @@ use Symfony\Component\Validator\Constraint;
 #[\Attribute]
 class ContainsMotCensor extends Constraint
 {
-    public string $message = 'Votre message contient des mots innaproprié';
-    // If the constraint has configuration options, define them as public properties
     public string $mode = 'strict';
+        public string $message = 'Votre message contient des mots innaproprié';
+    
+        // Define the default option
+        public string $defaultOption = 'mode';
+    
+        // If the constraint has configuration options, define them as public properties
+
 }
