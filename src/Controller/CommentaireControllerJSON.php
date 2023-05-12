6ed66faecb5e5089dabfc434d94d00f4fc9bb794
Abir-Kharmachi <abir.kharmachi@esprit.tn>
2023-05-12 12:45:50 +0200

    OK

--- src/Controller/CommentaireControllerJSON.php
+++ src/Controller/CommentaireControllerJSON.php
@@ -17,7 +17,7 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class CommentaireControllerJSON extends AbstractController
 {
-    #[Route('/commentaireJson/new', name: 'app_commentaire_newJson', methods: ['GET', 'POST'])]
+    #[Route('/json/commentaireJson/new', name: 'app_commentaire_newJson', methods: ['GET', 'POST'])]
     public function newcomm(UtilisateurRepository $u, Request $req, EntityManagerInterface $em, NormalizerInterface $Normalizer): Response
     {
         $commentaire = new Commentaire();
@@ -33,11 +33,11 @@ class CommentaireControllerJSON extends AbstractController
     #[Route('/commentaireJson/{id}', name: 'app_evaluation_showJson', methods: ['GET', 'POST'])]
     public function showAvis($id, CommentaireRepository $cr, UtilisateurRepository $ur, Request $req, EntityManagerInterface $em, NormalizerInterface $Normalizer): Response
     {
-        $u=$ur->find($id);
-        $comms=$cr->findByUser($id);
-        $jsonContent1=["evaluation"=>$u->getEvaluation()];
+        $u = $ur->find($id);
+        $comms = $cr->findByUser($id);
+        $jsonContent1 = ["evaluation" => $u->getEvaluation()];
         $jsonContent2 = $Normalizer->normalize($comms, 'json', ['groups' => "commentaire"]);
-        $res=[$jsonContent1,"messages"=>$jsonContent2];
+        $res = [$jsonContent1, "messages" => $jsonContent2];
         return new Response(json_encode($res));
     }
 
@@ -49,5 +49,4 @@ class CommentaireControllerJSON extends AbstractController
         $jsonContent = $Normalizer->normalize($c, 'json', ['groups' => 'commentaire']);
         return new Response("Commentaire supprimé " . json_encode($jsonContent));
     }
-
 }
